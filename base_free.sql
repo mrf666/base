80a4eee7d3f97318919522ddd4b7dadf0c8bd8ef
= <=>
2022-05-22 02:10:11 +0300

    ffs

--- base_free.sql
+++ base_free.sql
@@ -2,6 +2,8 @@ DROP TABLE items;
 DROP TABLE orders_from;
 DROP TABLE organization;
 DROP TABLE contact_database;
+DROP TABLE organization_status;
+DROP TABLE contact_person_post;
 
 CREATE Table orders_from(
 `id` int(5) auto_increment NOT NULL primary key,
@@ -16,31 +18,71 @@ CREATE TABLE items(
 FOREIGN KEY(`customer_id`) REFERENCES orders_from(`id`)
 );
 
+CREATE TABLE organization_status(
+`id` int(5) auto_increment NOT NULL primary key,
+`status` varchar(12) NOT NULL
+);
+
+
 CREATE TABLE organization(
 `id` int(5) auto_increment NOT NULL primary key,
-`item_id` int(5) NOT NULL,
+`order_id` int(5) NOT NULL,
 `organization_title` varchar(100) NOT NULL,
 `organization_field`varchar(100) NOT NULL,
+`organization_status` int(1) NOT NULL,
 `country` varchar(100) NOT NULL,
 `city` varchar(20) NOT NULL,
-FOREIGN KEY(`item_id`) REFERENCES items(`id`)
+FOREIGN KEY(`order_id`) REFERENCES orders_from(`id`),
+FOREIGN KEY(`organization_status`) REFERENCES organization_status(`id`)
 );
+
+
+CREATE TABLE contact_person_post(
+`id` int(5) auto_increment NOT NULL primary key,
+`place` varchar(30) NOT NULL
+);
+
+insert into contact_person_post(`place`) VALUES("Генеральный директор");
+insert into contact_person_post(`place`) VALUES("Менеджер");
+insert into contact_person_post(`place`) VALUES("Другое");
+
+
+CREATE TABLE contact_status(
+`id` int(5) auto_increment NOT NULL primary key,
+`status` varchar(17) NOT NULL
+);
+
+insert into contact_status(`status`) VALUES("Холодный");
+insert into contact_status(`status`) VALUES("Первичный контакт");
+insert into contact_status(`status`) VALUES("Ожидает звонка");
+insert into contact_status(`status`) VALUES("Ожидает КП");
+
+
 
 
 CREATE TABLE contact_database(
 `id` int(5) auto_increment NOT NULL primary key,
 `organization_id`int(5) NOT NULL,
-`item_id` int(5) NOT NULL,
+`order_id` int(5) NOT NULL,
+`name` varchar(30) NOT NULL,
+`place_id` int(5) NOT NULL,
 `number` varchar(25),
 `email` varchar(25),
+`status_id` int(5),
 FOREIGN KEY(`organization_id`) REFERENCES organization(`id`),
-FOREIGN KEY(`item_id`) REFERENCES items(`id`)
+FOREIGN KEY(`order_id`) REFERENCES orders_from(`id`)
+FOREIGN KEY(`place_id`) REFERENCES contact_person_post(`id`),
+FOREIGN KEY(`status_id`) REFERENCES contact_status(`id`)
 );
-
 
 insert into `orders_from`(`customer`,`find_field`) VALUES ('Подшибники', 'Фирмы по электрощитовому оборудованию');
 insert into `orders_from`(`customer`,`find_field`) VALUES ('Siemens','Склады и машиностроительные заводы');
 insert into `orders_from`(`customer`,`find_field`) VALUES ('petrol, oil and lubricants','Пленочные производства');
+
+
+insert into `organization_status`( `status`) VALUES('Неизвестно');
+insert into `organization_status`( `status`) VALUES('Не работает');
+insert into `organization_status`( `status`) VALUES('Работает');
 
 
 insert into `items`(`customer_id`,`title`) VALUES(2,'Test');
--- test_connection.py
+++ test_connection.py
@@ -2,7 +2,86 @@ import pymysql.cursors , pymysql.err
 import sys
 
 test_str = "SELECT * FROM orders_from"
-	
+conn = pymysql.connect(host='mysql.freehostia.com',user = 'serkal42_walls', password='crybabycry',db='serkal42_walls',cursorclass=pymysql.cursors.SSCursor)
+
+
+class database_obj:
+	obj_list = list()
+	"""docstring for database_obj"""
+	def __init__(self, dictonary):
+		self.dictonary = dictonary
+		obj_list.append(self.dictonary)
+		
+
+
+orders = (
+"id",
+"customer",
+"find_field"
+)
+
+#что то лишнее
+items = (
+"id",
+"customer_id",
+"title", 
+)
+
+organization_status = (
+"id",
+"status"
+)
+
+
+organization = (
+"id"
+"order_id"
+"organization_title" 
+"organization_field"
+"organization_status"
+"country" 
+"city"
+)
+
+
+contact_person_post = (
+"id",
+"place"
+)
+
+
+
+
+contact_database = (
+"id",
+"organization_id",
+"order_id" ,
+"name",
+"place_id" ,
+"number",
+"email",
+"status_id"
+)
+
+def add_item(customer_id,item):
+	try:
+		with conn.cursor() as cursor:
+			conn.autocommit(True)
+			print("connect")
+			add_item = f"insert into items({items[1]},{items[2]})VALUES(%s,%s)"
+			print(add_item)
+			cursor.execute(add_item,(str(customer_id),item))
+	except pymysql.err.OperationalError as e:
+		print(e)
+
+'''
+SELECT items.title, orders_from.customer,orders_from.find_field  
+FROM orders_from, items 
+WHERE orders_from.id = items.customer_id ;
+'''
+add_item("1","world")
+temp_list = list()
+
 try:
 	conn = pymysql.connect(host='mysql.freehostia.com',user = 'serkal42_walls', password='crybabycry',db='serkal42_walls',cursorclass=pymysql.cursors.SSCursor)
 	with conn.cursor() as cursor:
@@ -10,9 +89,12 @@ try:
 		conn.autocommit(True)
 		cursor.execute(test_str)
 		for row in cursor:
-			print(row)
-
-
+			temp_list.append(dict(zip(orders, row)))
+			print(temp_list)
+#
+#		for values in temp_list:
+#			new_dict = dict(zip(orders, temp_list))
+#			print(new_dict)
 
 
 except pymysql.err.OperationalError as e:
